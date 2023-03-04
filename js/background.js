9967bc06f1fc2406f324612816b7f33b5911587d
1g2g <wonji126326@gmail.com>
2023-03-04 21:03:31 +0900

    fix : random bgimg

--- js/background.js
+++ js/background.js
@@ -1,10 +1,14 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = [
+  "https://fastly.picsum.photos/id/460/2560/1440.jpg?grayscale&hmac=gYTlnU0vd5GFtaoRyYVN2qWZAr1Vgibfvn7DH78wPX8",
+  "https://fastly.picsum.photos/id/337/2560/1440.jpg?grayscale&hmac=Tr_1YjqGR-N0aIy1X20QxuSArGWJmcXXA-KatHlxdr0",
+  "https://fastly.picsum.photos/id/218/2560/1440.jpg?grayscale&hmac=qtEwJnLtYRO6kkKOyxMnG7C4g1s6JQjoxdo9Y3h4hL0",
+  "https://fastly.picsum.photos/id/419/2560/1440.jpg?grayscale&hmac=MbDcNzRvvPI65Z-ypGQRKdgLns3aWmM8iIqpmor2e_Q",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `https://picsum.photos/1000/1000?grayscale`;
+bgImage.src = `${chosenImage}`;
 
-document.body.style.backgroundImage =
-  "url('https://picsum.photos/2560/1440?grayscale')";
+document.body.style.backgroundImage = `url('${chosenImage}')`;
